908b7293417a121dad1e31dd5c6ce44e5abfb69b
Matheus Abreu <mabreu@PDM-00870.lan>
2019-03-21 22:53:52 +0000

    style: define login layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import logo from './logo.svg'
+import 'normalize.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <div className='login'>
+          <div className='logo'>
+            <img src={logo} alt='React Feed' />
+          </div>
+          <h1 className='title'>Log in to React Feed</h1>
+          <div className='field'>
+            <input
+              type='text'
+              name='username'
+              title='username'
+              id='username'
+              placeholder='Username'
+            />
+          </div>
+          <div className='field'>
+            <input
+              type='password'
+              name='password'
+              title='password'
+              id='password'
+              placeholder='Password'
+            />
+          </div>
+          <button className='button'>Log in</button>
+          <div className='actions'>
+            <a href='#a'>Recover Password</a>
+            <span> . </span>
+            <a href='#s'>Sign up for React Feed</a>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
